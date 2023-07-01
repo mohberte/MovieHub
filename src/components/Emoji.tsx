b0e41053ebdcdfbc66cc51b10ff8718faced8011
Mohamed Berte <flegpin@yahoo.fr>
2023-07-01 16:28:38 -0400

    code refactored

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -11,9 +11,9 @@ const Emoji = ({ rating }: Props) => {
   if (rating < 3) return null;
 
   const emojiMap: { [key: number]: ImageProps } = {
-    3: { src: meh, alt: 'meh', boxSize: '25px' },
-    4: { src: thumbsUp, alt: 'recommended', boxSize: '25px' },
-    5: { src: bullsEye, alt: 'exceptional', boxSize: '35px' },
+    6: { src: meh, alt: 'meh', boxSize: '25px' },
+    7: { src: thumbsUp, alt: 'recommended', boxSize: '25px' },
+    8: { src: bullsEye, alt: 'exceptional', boxSize: '35px' },
   }
 
   return (
